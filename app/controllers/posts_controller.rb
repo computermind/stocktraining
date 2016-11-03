de8431fdce80a47b086329ed9defde67e4ccdb94
Thomas Guwor <guworthomas@gmail.com>
2016-11-03 05:23:43 +0000

    Add new and create actions and view,  and install simple_form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
     def index
     end
+    
+    def show
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.find(post_params)
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    private
+        def post_params
+        end
 end
