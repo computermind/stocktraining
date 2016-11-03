86691dbb37e028598e64849e540ee8777b47beb1
Thomas Guwor <guworthomas@gmail.com>
2016-11-03 23:31:10 +0000

    Add Posts controller, CRUD and Views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
     def index
+        @posts = Post.all
     end
     
     def show
@@ -10,19 +12,35 @@ class PostsController < ApplicationController
     end
     
     def create
-        @post = Post.find(post_params)
+        if @post = Post.create(post_params)
+            redirect_to posts_path
+        else
+            render :new
+        end
     end
     
     def edit
     end
     
     def update
+        if @post = Post.update(post_params)
+            redirect_to posts_path
+        else
+            render :edit
+        end
     end
     
     def destroy
+        @post.destroy
+        redirect_to root_path
     end
     
     private
         def post_params
+            params.require(:post).permit(:image, :caption)
+        end
+        
+        def post_find
+            @post = Post.find(params[:id])
         end
 end
