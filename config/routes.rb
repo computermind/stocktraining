ba1e3e667d182387f935a7d7071399d7b01b7a91
Thomas Guwor <guworthomas@gmail.com>
2016-11-06 05:32:04 +0000

    Add comment model and associations

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 end
