4c45fe58c7bfb8bacbc4266f239017973d2dde7d
Thomas Guwor <guworthomas@gmail.com>
2016-11-07 18:57:45 +0000

    Add profile path to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   root 'landing#index'
+  
+get ':user_name', to: 'profiles#show', as: :profile
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   
