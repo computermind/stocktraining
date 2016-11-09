35c10d287177da6a38a5a490e699995c6bebd6ff
Thomas Guwor <guworthomas@gmail.com>
2016-11-09 19:23:18 +0000

    Add browser for newsfeed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   post ':user_name/follow_user', to: 'relationships#follow_user', as: :follow_user
   post ':user_name/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
 
+  get 'browse', to: 'posts#browse', as: :browse_posts  
+  
   get 'relationships/follow_user'
 
   get 'relationships/unfollow_user'
