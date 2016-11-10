8d5b2bd1e946da16c54fb7568f4bda6413dc6e15
Thomas Guwor <guworthomas@gmail.com>
2016-11-10 15:14:53 +0000

    Style landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,3 +1,12 @@
 class LandingController < ApplicationController
-skip_before_action :authenticate_user!
+    skip_before_action :authenticate_user!
+    before_action :check_login, only: :index
+
+private
+
+    def check_login
+        if user_signed_in?
+            redirect_to browse_posts_path
+        end
+    end
 end
