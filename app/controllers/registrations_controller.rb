865fe6bc5fae898c8b69c4ca7927a20f61d17994
Thomas Guwor <guworthomas@gmail.com>
2016-11-10 15:06:40 +0000

    Update login/signup redirects

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
 
   private
-
+  
+  def after_sign_up_path_for(resource)
+    browse_posts_path
+  end
+  
   def sign_up_params
     params.require(:user).permit(:email, :user_name, :password, :password_confirmation)
   end
