1224c79e82c4566fe160860ecb339524e6a9b6be
Thomas Guwor <guworthomas@gmail.com>
2016-11-14 19:27:44 +0000

    Update post model to usder_id validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
     
     validates :image, presence: true
     validates :user_id, presence: true
-#    validates :caption, presence: true, length: { minimum: 4, maximum: 300 }
+#   validates :caption, presence: true, length: { minimum: 4, maximum: 300 }
     
     has_attached_file :image, styles: { :medium => "640x" }
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-    has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
   has_many :followers, through: :follower_relationships, source: :follower
 
   has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
